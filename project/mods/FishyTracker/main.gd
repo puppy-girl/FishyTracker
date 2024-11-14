f1977ccbdd926cbc0acf1289a83328225de85a7c
Jade ・:*・。*・ <asleepydoggy@gmail.com>
2024-11-14 15:16:39 +1100

    feat: save and read fish log from file

--- project/mods/FishyTracker/main.gd
+++ project/mods/FishyTracker/main.gd
@@ -1,16 +1,27 @@
 extends Node
 
-onready var _last_inventory_size: int = PlayerData.inventory.size()
-
 var fish_log: Array = [] setget _set_nullifier
 
-
-func _init() -> void:
-	pass
+var _last_inventory_size: int = 0 setget _set_nullifier
 
 
 func _ready() -> void:
+	get_tree().connect("node_added", self, "_on_node_added")
 	PlayerData.connect("_inventory_refresh", self, "_on_inventory_update")
+	UserSave.connect("_slot_saved", self, "_save_fish_logs")
+	
+	var save_slot = UserSave.current_loaded_slot
+	if save_slot != -1: # loaded slot is -1 when there's no save file
+		_load_fish_logs(save_slot)
+
+
+func _on_node_added(node: Node) -> void:
+	if (
+			node.name == "save_select"
+			or node.name.begins_with("@save_select@")
+			and node.get_parent().name != "main_menu"
+	):
+		node.connect("_pressed", self, "_switch_save_slot", [], CONNECT_DEFERRED)
 
 
 func _on_inventory_update() -> void:
@@ -28,7 +39,7 @@ func _on_inventory_update() -> void:
 	for i in new_entries:
 		var entry_index: int = inventory_size - new_entries + i
 		var entry: Dictionary = PlayerData.inventory[entry_index]
-		var is_fish: bool = entry.id.begins_with("fish")
+		var is_fish: bool = entry.id.begins_with("fish_")
 	
 		if (
 				fish_log_size > 0
@@ -38,6 +49,43 @@ func _on_inventory_update() -> void:
 	
 		if is_fish:
 			fish_log.append(entry)
+
+
+func _save_fish_logs() -> void:
+	var save_slot: int = UserSave.current_loaded_slot
+	var save: Dictionary = {
+		"fish_log": fish_log,
+		"inventory_size": _last_inventory_size
+	}
+	
+	var dir := Directory.new()
+	if !dir.dir_exists("user://FishyTracker"):
+		dir.make_dir("user://FishyTracker")
+	
+	var file := File.new()
+	file.open("user://FishyTracker/fish_log_slot_%s.dat" % save_slot, File.WRITE)
+	file.store_string(JSON.print(save))
+	file.close()
+
+
+func _load_fish_logs(save_slot: int) -> void:
+	var file := File.new()
+	file.open("user://FishyTracker/fish_log_slot_%s.dat" % save_slot, File.READ)
+	var content := file.get_as_text()
+	file.close()
+	
+	var stored_json := JSON.parse(content)
+	if stored_json.error == OK:
+		fish_log = stored_json.result.fish_log
+		_last_inventory_size = stored_json.result.inventory_size
+	
+	_last_inventory_size = PlayerData.inventory.size()
+
+
+func _switch_save_slot() -> void:
+	var save_slot: int = UserSave.current_loaded_slot
+	
+	_load_fish_logs(save_slot)
 
 
 func _set_nullifier(_v) -> void:
